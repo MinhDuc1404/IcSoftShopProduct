0385eecd3b0cdaf4bc84e812bb02fae0a2b032b9
Admin <Admin@ADMIN>
2024-12-23 15:06:38 +0700

    update 23/12/2024

--- IcSoftShopAdmin/Program.cs
+++ IcSoftShopAdmin/Program.cs
@@ -41,8 +41,11 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 
 builder.Services.AddDefaultIdentity<ShopUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-    builder.Services.AddHttpClient();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager<SignInManager<ShopUser>>();
+
+
+builder.Services.AddHttpClient();
     builder.Services.AddScoped<IProductServices, ProductServices>();
     builder.Services.AddScoped<ICategoryServices, CategoryServices>();
     builder.Services.AddScoped<ICollectionServices, CollectionServices>();
